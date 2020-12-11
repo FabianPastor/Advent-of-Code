6e87da9238e4bf0bc0d3772602e23c99bc672d3c
FabianPastor <fabian_pastor@msn.com>
2020-12-11 16:37:43 +0100

    Day8 Part 1: Finished!

--- Day8/part1.php
+++ Day8/part1.php
@@ -2,6 +2,37 @@
 $example = "example_input.txt";
 $puzzle = "puzzle_input.txt";
 
-$filename = $example;
+$filename = $puzzle;
 
 $input = trim(file_get_contents($filename));
+
+$instructions = array_map("trim",explode(PHP_EOL,$input));
+
+//Global Variables
+$accumulator = 0;
+
+$totalInstructions = count($instructions);
+for($pointer = 0,$i = 0; $pointer < $totalInstructions; $pointer++,$i++){
+  
+  list($instruction,$operation) = explode(" ",$instructions[$pointer]);
+  echo "$pointer | $instructions[$pointer] | $accumulator".PHP_EOL;
+  switch($instruction){
+    case "acc":
+      $accumulator += $operation;
+    break;
+    
+    case "jmp":
+      $pointer += $operation-1;
+    break;
+    
+    case "nop":
+      
+    break;
+    
+    default:
+      echo "Unknown instruction ".$instructions[$pointer].PHP_EOL;
+    
+  }
+  if($i>1000) break;
+  
+}

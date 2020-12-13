3b12a8195d13695edcfdd511e79eb2172249ffd7
FabianPastor <fabian_pastor@msn.com>
2020-12-13 23:38:53 +0100

    Day12 Part1: Added some stupid things :D

--- Day12/part1.php
+++ Day12/part1.php
@@ -2,7 +2,7 @@
 $example       = "example_input.txt";
 $puzzle        = "puzzle_input.txt";
 
-$filename = $puzzle;
+$filename = $example;
 
 $input = trim(file_get_contents($filename));
 
@@ -11,10 +11,14 @@ $instructions = array_map("trim",explode(PHP_EOL, $input));
 
 $verbose = false;
 $ship = new Ship($verbose);
+$ship->drawGrid();
 foreach($instructions as $instruction){
+  echo " -------------------------------- ".PHP_EOL;
   $ship->executeInstruction($instruction);
+  $ship->drawGrid();
 }
-$ship->draw();
+echo " -------------------------------- ".PHP_EOL;
+$ship->print();
 
 
 
@@ -46,6 +50,12 @@ class Cardinal{
     SELF::SOUTH => Direction::SOUTH,
     SELF::WEST  => Direction::WEST,
   ];
+  const EMOJI = [
+    SELF::NORTH => "👆",
+    SELF::EAST  => "👉",
+    SELF::SOUTH => "👇",
+    SELF::WEST  => "👈",
+  ];
 }
 // class CardinalText{
 //   const NORTH   = "N";
@@ -56,6 +66,12 @@ class Cardinal{
 
 
 class Ship{
+  private $gridSize = 20;
+  private $x = 0;
+  private $y = 0;
+  private $grid = [];
+  
+  
   private $current_direction = Cardinal::EAST;
   private $moved = [
     Direction::NORTH => 0,
@@ -67,6 +83,34 @@ class Ship{
   
   public function __construct($verbose = false){
     $this->v = $verbose;
+    $this->initGrid();
+    
+  }
+  
+  public function initGrid(){
+    
+    for( $i = 0; $i < $this->gridSize ; $i++  ){
+      for( $j = 0; $j < $this->gridSize ; $j++ ){
+          $this->grid[$i][$j] = ".";
+      }
+    }
+    $this->grid[$this->y][$this->x] = "#";
+  }
+  
+  public function updateGrid($y, $x, $direction = false){
+    
+    for( $i = 0; $i < $this->gridSize ; $i++  ){
+      for( $j = 0; $j < $this->gridSize ; $j++ ){
+        if($this->grid[$i][$j] == "#"){
+          if($direction !== false){
+            $this->grid[$i][$j] = Cardinal::EMOJI[$direction];
+          }
+        }
+      }
+    }
+    $this->y = $y;
+    $this->x = $x;
+    $this->grid[$y][$x] = "#";
   }
   
   public function executeInstruction($instruction){
@@ -112,16 +156,45 @@ class Ship{
     
     if($this->v) echo "  Current Direction: ".Cardinal::TEXT[$current_direction]." moving {$ammount}".PHP_EOL;
     $this->moved[Cardinal::TEXT[$current_direction]] += $ammount;
+    
+    $this->moveOnGrid($current_direction, $ammount);
+    
     if($this->v) {
-      $this->draw();
+      $this->print();
       echo PHP_EOL.PHP_EOL;
     }
+  }
+  
+  public function moveOnGrid($direction, $ammount){
+    
+    $x = $this->x;
+    $y = $this->y;
+    switch($direction){
+      case Cardinal::NORTH:
+        $y -= $ammount;
+        if($y < 0) $y = 0;
+      break;
+      case Cardinal::SOUTH:
+        $y += $ammount;
+        if($y > $this->gridSize) $y = ($this->gridSize-1);
+      break;
+      
+      case Cardinal::EAST:
+        $x += $ammount;
+        if($x > $this->gridSize) $x = ($this->gridSize-1);
+      break;
+      case Cardinal::WEST:
+        $x -= $ammount;
+        if($x < 0) $x = 0;
+      break;
+    }
+    $this->updateGrid($y, $x, $direction);
   }
   public function getPosition(){
     return abs($this->moved[Direction::NORTH] - $this->moved[Direction::SOUTH]) + abs($this->moved[Direction::EAST] - $this->moved[Direction::WEST]);
   }
   
-  public function draw(){
+  public function print(){
     echo "┏━━━━━━━━━━━━━━━━┓".PHP_EOL;
     echo "┃ ".sprintf( "%-8s %-5s %s" , "North: ", $this->moved[Direction::NORTH],"┃").PHP_EOL;
     echo "┃ ".sprintf( "%-8s %-5s %s" , "South: ", $this->moved[Direction::SOUTH],"┃").PHP_EOL;
@@ -129,6 +202,87 @@ class Ship{
     echo "┃ ".sprintf( "%-8s %-5s %s" , "West: ",  $this->moved[Direction::WEST ],"┃").PHP_EOL;
     echo "┗━━━━━━━━━━━━━━━━┛".PHP_EOL.PHP_EOL;
     echo "E/W + N/S Relation : ".$this->getPosition().PHP_EOL;
+  }
+  
+  public function drawGrid(){
+    foreach($this->grid as $line){
+      $cells = [];
+      foreach($line as $cell){
+        switch($cell){
+          case "#":
+            $cells = $this->mergeCells($cells, $this->cellShip());
+          break;
+          case ".":
+            $cells = $this->mergeCells($cells, $this->cellEmpty());
+          break;
+          
+          case Cardinal::EMOJI[Cardinal::NORTH]:
+          $cells = $this->mergeCells($cells, $this->cellDirection(Cardinal::NORTH));
+        break;
+          case Cardinal::EMOJI[Cardinal::EAST]:
+          $cells = $this->mergeCells($cells, $this->cellDirection(Cardinal::EAST));
+        break;
+          case Cardinal::EMOJI[Cardinal::SOUTH]:
+          $cells = $this->mergeCells($cells, $this->cellDirection(Cardinal::SOUTH));
+        break;
+          case Cardinal::EMOJI[Cardinal::WEST]:
+            $cells = $this->mergeCells($cells, $this->cellDirection(Cardinal::WEST));
+          break;
+        }
+      }
+      $this->drawCellLine($cells);
+    }
+  }
+  
+  public function mergeCells($a, $b){
+    if(empty($a)){
+      return $b;
+    }
+    $str = [];
+    foreach($a as $i => $line){
+      $str[] = $a[$i].$b[$i];
+    }
+    return $str;
+    
+  }
+  
+  public function drawCellLine($cells){
+    echo implode(PHP_EOL,$cells).PHP_EOL;
+  }
+  
+  public function cellShip(){
+    
+    $d = $this->current_direction;
+    $emoji = Cardinal::EMOJI[$d];
+    
+    $str    = [];
+    $str [] =        "┏━━━━━━━━┓";
+    $str [] = sprintf( "┃%2s %-2s %2s┃" , ""                                  , (($d == Cardinal::NORTH)?$emoji:"") , ""                                                     );
+    $str [] = sprintf( "┃%2s %-2s %2s┃" , (($d == Cardinal::WEST)?$emoji:"")  , "⛵️"                                 , (($d == Cardinal::EAST)?$emoji:"") );
+    $str [] = sprintf( "┃%2s %-2s %2s┃" , ""                                  , (($d == Cardinal::SOUTH)?$emoji:"") , ""                                                    );
+    $str [] =        "┗━━━━━━━━┛";
+    
+    return $str;
+  }
+  public function cellEmpty(){
+    $str    = [];
+    $str [] =        "┏━━━━━━━━┓";
+    $str [] =  sprintf( "┃%2s %-2s %2s┃" , ""  , "" , "");
+    $str [] =  sprintf( "┃%2s %-2s %2s┃" , ""  , "" , "");
+    $str [] =  sprintf( "┃%2s %-2s %2s┃" , ""  , "" , "");
+    $str [] =         "┗━━━━━━━━┛";
+    return $str;
+  }
+  public function cellDirection($direction){
+    $emoji = Cardinal::EMOJI[$direction];
+    
+    $str    = [];
+    $str [] =        "┏━━━━━━━━┓";
+    $str [] =  sprintf( "┃%2s %-2s %2s┃" , ""  , "" , "");
+    $str [] =  sprintf( "┃%2s %-2s %2s┃" , ""  , "$emoji" , "");
+    $str [] =  sprintf( "┃%2s %-2s %2s┃" , ""  , "" , "");
+    $str [] =         "┗━━━━━━━━┛";
+    return $str;
   }
   
   

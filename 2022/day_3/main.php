9dcc4c99ee072710ec0b491c01d7aade4c95c93f
FabianPastor <fabian_pastor@msn.com>
2022-12-03 15:19:34 +0100

    Refactored day 3

--- 2022/day_3/main.php
+++ 2022/day_3/main.php
@@ -3,130 +3,53 @@
 require_once "../includes/functions.php";
 $inputs = array_map("trim",explode("\n",trim($input)));
 
-
-$rucksacks = [];
-$priorities = [
-  "content" => [],
-  "sum" => 0,
-];
-
-foreach($inputs as $index => $item){
-  $rucksack = [];
-  $rucksack["items"] = $item;
-  $len = strlen($item)/2;
-  $rucksack[1] = [
-    "text" => substr($item,0,$len),
-    "content"=>[],
-    // "sum" => 0,
-  ];
-  $rucksack[2] = [
-    "text" => substr($item,$len),
-    "content"=>[],
-    // "sum" => 0,
-  ];
-  
-  
-  $items = str_split($item);
-  // echo $rucksack[1]["text"].PHP_EOL;
-  $current = &$rucksack[1]["content"];
-  $changed = false;
-  $priorityFound = false;
-  foreach($items as $i => $char){
-    if(!$changed && $i >= $len) {
-      // echo $rucksack[2]["text"].PHP_EOL;
-      $current = &$rucksack[2]["content"];
-      $changed = true;
-    }
-    
-    $value = $char;
-    $current[] = $value;
-    
-    if($changed){
-      // $rucksack[2]["sum"] += $value;
-      if(in_array($value, $rucksack[1]["content"])){
-        $rucksack["same"]["$char"] = getValue($char);
-        if(!$priorityFound){
-          $priorityFound = true;
-          $priorities["content"][] = $rucksack["same"]["$char"];
-          $priorities["sum"] += $rucksack["same"]["$char"];
-        }
-      }
-    }else{
-      // $rucksack[1]["sum"] += $value;
-    }
-    
-
-  }
-  unset($current);
-
-
-  $rucksacks[] = $rucksack;
-  
-}
-
-$group = 0;
-$groups = [];
-$groups[0]["elements"] = [];
-$priorities = [
-  "content" => [],
-  "sum" => 0,
-];
-foreach($rucksacks as $index => $rucksack){
-  // var_dump($rucksack);
-  $current = &$groups[$group];
-  
-  $chars = str_split($rucksack["items"]);
-  $elements = [];
-  foreach($chars as $char){
-    if(!isset($elements[$char])){
-      $elements[$char] = 1;
-    }
-  }
-  
-  foreach($elements as $char => $amount){
-    if(!isset($current["elements"]["$char"])){
-      $current["elements"]["$char"] = 1;
-    }else{
-      $current["elements"]["$char"]++;
-    }
-  }
-  
-  
-  
-  
-  // $current[]
-  
-  
-  unset($current);
-  if($index%3===2){
-    
-    $char = array_search(3,$groups[$group]["elements"]);
-    $value = getValue($char);
-    $priorities["content"][] = $char;
-    $priorities["sum"] += $value;
-    
-    
-    // $priorities["sum"] 
-    
-    
-    
-    
-    
-    $group++;
-    $groups[$group]["elements"] = [];
-  }
-}
-unset($groups[$group]);
-
-// foreach($rucksacks as $rucksack){
-//   echo json_encode($rucksack).PHP_EOL;
-// }
-// var_dump($rucksacks,$priorities);
-// var_dump($groups);
-echo "Number: ".$priorities["sum"].PHP_EOL;
-
 function getValue(string $char):int{
   $ord = ord($char);
-
-  return $ord - (($ord>90)?96:(65-27));
+  // return 1 + $ord - ( ($ord >= ord("a") )?ord("a"):ord("A")-26 );
+  // return 1 + $ord - (($ord >= 97)?97:(65-26));
+  return $ord - (($ord >= 97)?96:38);
 }
+
+
+$priorities = 0;
+foreach($inputs as $index => $rucksack){
+  $rucksack = trim($rucksack);
+  $n = strlen($rucksack)/2;
+  $items = str_split($rucksack);
+  $items = array_map("getValue",$items);
+  
+  $list1 = array_slice($items, 0, $n);
+  $list2 = array_slice($items, $n);
+  
+  foreach($list1 as $item){
+    if(in_array($item, $list2)){
+      $priorities += $item;
+      break;
+    }
+  } 
+}
+
+echo "Part1 Priorities: ".$priorities.PHP_EOL;
+
+
+$priorities = 0;
+$groupItems = [];
+foreach($inputs as $index => $rucksack){
+  $items = str_split(trim($rucksack));
+  $items = array_unique(array_map("getValue",$items));
+  $groupItems = array_merge($groupItems, $items);
+  
+  if($index%3===2){
+    $elements = [];
+    foreach($groupItems as $key => $item){
+      $elements[$item] ??= 0;
+      $elements[$item]++;
+    }
+    $i = array_search(3,$elements);
+    $priorities += $i;
+    
+    $groupItems = [];
+  }
+}
+
+echo "Part2 Priorities: ".$priorities.PHP_EOL;

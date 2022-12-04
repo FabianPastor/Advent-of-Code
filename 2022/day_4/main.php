b52ee02cd3677c32b4d66f7ab173b6dcc7baf6b7
FabianPastor <fabian_pastor@msn.com>
2022-12-04 11:02:58 +0100

    Minor changes

--- 2022/day_4/main.php
+++ 2022/day_4/main.php
@@ -3,30 +3,29 @@
 require_once "../includes/functions.php";
 $inputs = array_map("trim",explode("\n",trim($input)));
 
+function isPartiallyContainedPart2($a, $b){
+  if(isFullyContainedPart1($a,$b)) return true;
+  return ( (($a[0] <= $b[0] && $b[0] <= $a[1]) && ($a[1] < $b[1])) ||
+           (($b[0] <= $a[0] && $a[0] <= $b[1]) && ($b[1] < $a[1]))  );
+}
+
+function isFullyContainedPart1($a, $b){
+  return ( ($a[0] <= $b[0] && $b[1] <= $a[1]) ||
+           ($b[0] <= $a[0] && $a[1] <= $b[1])  );
+}
+
+
 $assignements = 0;
 foreach($inputs as $pairedElfs){
   [$elf1, $elf2] = explode(",",$pairedElfs);
   [$elf1Min, $elf1Max] = explode("-",$elf1);
   [$elf2Min, $elf2Max] = explode("-",$elf2);
   
-  if(isPartiallyContainedPart2([$elf1Min, $elf1Max],[$elf2Min, $elf2Max])){
+  if(isPartiallyContainedPart2(
+    [$elf1Min, $elf1Max],
+    [$elf2Min, $elf2Max]
+  )){
     $assignements++;
   }
-  
-  
 }
-
-echo "Assignements fully contain the other: ".$assignements.PHP_EOL;
-
-function isPartiallyContainedPart2($a, $b){
-  
-  if(isFullyContainedPart1($a,$b)) return true;
-  return ( (($a[0] <= $b[0] && $b[0] <= $a[1]) && ($a[1] < $b[1])) ||
-           (($b[0] <= $a[0] && $a[0] <= $b[1]) && ($b[1] < $a[1]))  );
-   
-}
-
-function isFullyContainedPart1($a, $b){
-  return (($a[0]<=$b[0] && $b[1]<=$a[1]) ||
-          ($b[0]<=$a[0] && $a[1]<=$b[1]));
-}
+echo "Assignements: ".$assignements.PHP_EOL;
